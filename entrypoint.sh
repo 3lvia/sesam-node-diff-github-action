38de3b6ca2ee0a533cc4f5222207d930d7398e34
mofsesam <morten.frellumstad@sesam.io>
2024-02-23 17:27:39 +0100

    Fix entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,20 +2,23 @@
 
 if $INPUT_DOWNLOAD = "true"; then
   echo "Downloading config from $INPUT_NODE"
-  curl 'https://$INPUT_NODE/api/config' \
+  curl "https://$INPUT_NODE/api/config" \
   -H 'accept: application/zip' \
-  -H 'authorization: bearer $INPUT_JWT' \
+  -H "authorization: bearer $INPUT_JWT" \
   --compressed \
-  -o sesam_download_config.zip && \
-    unzip sesam_download_config.zip -d /tmp/sesam_download_config && \
-    mv /tmp/sesam_download_config/* $INPUT_CONFIG_PATH_DOWNLOAD && \
-    rm -rf /tmp/sesam_download_config
+  -o /tmp/sesam_download_config.zip 
+
+    unzip /tmp/sesam_download_config.zip -d /tmp/sesam_download_config 
+    mkdir -p ./$INPUT_CONFIG_PATH_DOWNLOAD
+    cp /tmp/sesam_download_config/* ./$INPUT_CONFIG_PATH_DOWNLOAD 
+    rm -rf /tmp/sesam_download_config /tmp/sesam_download_config.zip 
 else
   echo "Uploading config to Sesam"
 fi
 
-git diff --no-index $@ -- $INPUT_CONFIG_PATH_LOCAL $INPUT_CONFIG_PATH_DOWNLOAD
+git diff --no-index $@ -- $INPUT_CONFIG_PATH_LOCAL/ $INPUT_CONFIG_PATH_DOWNLOAD/
 
 # If there are differences, git exits with 1. We want to ignore this and give exit code 0 back to github actions
 
 exit 0
+
